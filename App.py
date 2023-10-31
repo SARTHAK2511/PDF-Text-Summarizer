57a64b992a49f3c460457b59776103a238dfac0c
SaRTaK_B <90795608+SARTHAK2511@users.noreply.github.com>
2023-10-31 19:15:17 +0530

    Update App.py

--- App.py
+++ App.py
@@ -8,9 +8,9 @@ import torch
 import base64
 
 #model and tokenizer loading
-checkpoint = "4e871ba5f20216feaa3b845fc782229cd64eba47"
+checkpoint = "LaMini-Flan-T5-248M"
 tokenizer = T5Tokenizer.from_pretrained(checkpoint)
-base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32,offload_folder='offload')
+base_model = T5ForConditionalGeneration.from_pretrained(checkpoint, device_map='auto', torch_dtype=torch.float32)
 
 #file loader and preprocessing
 def file_preprocessing(file):
@@ -30,7 +30,7 @@ def llm_pipeline(filepath):
         'summarization',
         model = base_model,
         tokenizer = tokenizer,
-        max_length = 1000, 
+        max_length = 500, 
         min_length = 50)
     input_text = file_preprocessing(filepath)
     result = pipe_sum(input_text)
@@ -54,16 +54,16 @@ def displayPDF(file):
 st.set_page_config(layout="wide")
 
 def main():
-    # st.title("Document Summarization App using Langauge Model")
+    st.title("Document Summarization App using Langauge Model")
 
     uploaded_file = st.file_uploader("Upload your PDF file", type=['pdf'])
 
     if uploaded_file is not None:
         if st.button("Summarize"):
             col1, col2 = st.columns(2)
-            filepath = r"C:\Users\HP\Downloads\Sarthak_CV (1).pdf"
-            # with open(filepath, "wb") as temp_file:
-            #     temp_file.write(uploaded_file.read())
+            filepath = "data/"+uploaded_file.name
+            with open(filepath, "wb") as temp_file:
+                temp_file.write(uploaded_file.read())
             with col1:
                 st.info("Uploaded File")
                 pdf_view = displayPDF(filepath)
